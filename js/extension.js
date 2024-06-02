3ee20e0a18797ae72c0b2ab50362698bb6d4cfd6
AntonellaWeb <157330839+AntonellaWeb@users.noreply.github.com>
2024-06-02 16:10:35 -0300

    yes

--- js/extension.js
+++ js/extension.js
@@ -1,4 +1,9 @@
-var Nombre; "Anto"
-var Edad; "si"
+var nombre = "Anto";
+var edad = "18";
 
-alert(Quieres Viajar + Nombre);
+alert("Quieres Viajar " + nombre);
+
+if (true) {
+    edad > 18
+}
+console.log (puedes reservar hoteles);
