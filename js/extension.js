a96fc66ab9cdaa0b1246a822ba0874b0456118c0
AntonellaWeb <157330839+AntonellaWeb@users.noreply.github.com>
2024-06-28 21:41:26 -0300

    yes

--- js/extension.js
+++ js/extension.js
@@ -2,38 +2,38 @@
 const producto = [
     {
         id: 1,
-        nombre: "Ce",
-        precio: 15000,
+        nombre: "Samsung",
+        precio: 150000,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuc6f2A3_c8LJJOqRvcF6SNsGw0xCUf9qnPA&s",
     },
     {
         id: 2,
-        nombre: "Celular",
-        precio: 15000,
+        nombre: "Motorola",
+        precio: 100000,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZ2a3AZj25d8XD12V24m5ZyyL6MjEQ8hoeQA&s",
     },
     {
         id: 3,
-        nombre: "Heladera",
-        precio: 15000,
+        nombre: "Apple",
+        precio: 250000,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPYkpU17XqjizFFHNPGwXosG4Ium62Rde2sg&s",
     },
     {
         id: 4,
-        nombre: "Televisor",
-        precio: 15000,
+        nombre: "Xiamo",
+        precio: 150000,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Mm6BKVaoaRhi1vHuiMvBNjDGeOqvfkNzpA&s",
     },
     {
         id: 5,
-        nombre: "Celular",
-        precio: 15000,
+        nombre: "Redmi",
+        precio: 205000,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQop7bdJ_eDZdEIf5vxGJfpeCi2R14ksjhzA&s",
     },
     {
         id: 6,
-        nombre: "Heladera",
-        precio: 15000,
+        nombre: "Huawei",
+        precio: 150000,
         img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaa4BAnsOSFB0LkTp6cdQ_ZGw4InILYv286Q&s",
     },
 ];
@@ -45,9 +45,9 @@ function renderProducto (productsArray){
     productsArray.forEach(producto => {
         const card = document.createElement("div")
         card.innerHTML = `<img class="imagen" src="${producto.img} width="350px" alt="Producto 1">
-                          <h2 class="detalles">${producto.nombre}</h2>
-                          <p class="Precio">${producto.precio}$</p>
-                          <button class= "productoAgregar" id="${producto.id}">Comprar </button>`
+                          <h2 class="informacion">${producto.nombre}</h2>
+                          <p class="informacion-precio">${producto.precio}$</p>
+                          <button class= "productoAgregar" id="${producto.id}">Comprar</button>`
         productsContainer.appendChild(card);
     });
     addToCardButton()
@@ -67,3 +67,15 @@ function addToCardButton (){
         }
     })
 };
+
+const header = document.querySelector("#header");
+const contenedor = document.querySelector("#contenedor");
+const body = document.querySelector("body");
+window.addEventListener("scroll", function(){
+    if(contenedor.getBoundingClientRect().top<10){
+        header.classList.add("scroll")
+    }
+    else{
+        header.classList.remove("scroll")
+    }
+});
