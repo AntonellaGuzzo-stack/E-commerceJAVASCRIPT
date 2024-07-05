6cc9fda30e636871b92e942873cc180ce094cf1e
AntonellaWeb <157330839+AntonellaWeb@users.noreply.github.com>
2024-07-04 21:27:54 -0300

    yes

--- js/carrito.js
+++ js/carrito.js
@@ -1,44 +1,87 @@
-let celularStorage = localStorage.getItem("celularProducts");
-celularStorage = JSON.parse(celularStorage);
-let celularContainer = document.getElementById("celulares-section");
 
-function renderCarrito (celularItems){
-  celularItems.forEach(producto => {
-      const celular = document.createElement("div")
-      celular.classList = "tarjeta-producto"
-      celular.innerHTML = `<p id="${producto.id}></p>
-                        <h2 class="cantidad">${producto.nombre}</h2>
-                        <div><button class= "productoRestar">-</button>
-                        <p class="Precio">${producto.precio}</p>
-                        <button class= "productoSumar">+</button></div>`;                 
-      celularContainer.appendChild(celular);
-  })
-  RestarProducto()
-}
-renderCarrito(celularStorage);
 
-function RestarProducto() {
-  const restarButtons = document.querySelectorAll(".productoRestar");
-  restarButtons.forEach(button => {
-    button.onclick = (e) => {
-      const productId = e.currentTarget.id;
-      const selectedProduct = producto.find(producto => producto.id == productId);
+carrito.forEach((producto) => {
+    let carritoContent = document.createElement("div");
+    carritoContent.className = "modal-content";
+    carritoContent.innerHTML = `
+        <img src="${producto.img}">
+        <h3>${producto.nombre}</h3>
+        <p>${producto.precio} $</p>
+        <span class="restar"> - </span>
+        <!--recomiendo no escribir la palabra cantidad para que no quede tan largo :)-->
+        <p>${producto.cantidad}</p>
+        <span class="sumar"> + </span>
+        <p>Total: ${producto.cantidad * producto.precio} $</p>
+        <span class="delete-product"> ❌ </span>
+      `;
 
-      // Verifica si el producto ya está en el arreglo
-      const existingProductIndex = celularProducts.findIndex(p => p.id === selectedProduct.id);
-      if (existingProductIndex !== -1) {
-        // Resta la cantidad
-        celularProducts[existingProductIndex].cantidad -= 1;
+    modalContainer.append(carritoContent);
 
-        // Si la cantidad llega a cero, elimina el producto del arreglo
-        if (celularProducts[existingProductIndex].cantidad === 0) {
-          celularProducts.splice(existingProductIndex, 1);
-        }
+    let restar = carritoContent.querySelector(".restar");
+
+    restar.addEventListener("click", () => {
+      if (producto.cantidad !== 1) {
+        producto.cantidad--;
       }
+      saveLocal();
+      pintarCarrito();
+    });
 
-      // Actualiza la interfaz y los totales
-      crearTarjetasProductosCarrito();
-      actualizarTotales();
-    };
+    let sumar = carritoContent.querySelector(".sumar");
+    sumar.addEventListener("click", () => {
+      producto.cantidad++;
+      saveLocal();
+      pintarCarrito();
+    });
+
+    let eliminar = carritoContent.querySelector(".delete-product");
+
+    eliminar.addEventListener("click", () => {
+      eliminarProducto(producto.id);
+    });
+
+    // let eliminar = document.createElement("span");
+    // eliminar.innerText = "❌";
+    // eliminar.classList = "delete-product";
+    // carritoContent.append(eliminar);
+
+    // eliminar.addEventListener("click", eliminarProducto);
   });
-}
+
+  const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+
+  const totalBuying = document.createElement("div");
+  totalBuying.className = "total-content";
+  totalBuying.innerHTML = `Total a pagar: ${total} $`;
+  modalContainer.append(totalBuying);
+
+
+verCarrito.addEventListener("click", () => {
+  window.location.href = "carrito/carrito.html";
+});
+
+const eliminarProducto = (id) => {
+  const foundId = carrito.find((element) => element.id === id);
+
+  console.log(foundId);
+
+  carrito = carrito.filter((carritoId) => {
+    return carritoId !== foundId;
+  });
+
+  carritoCounter();
+  saveLocal();
+  pintarCarrito();
+};
+
+const carritoCounter = () => {
+  cantidadCarrito.style.display = "block";
+
+  const carritoLength = carrito.length;
+
+  localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
+
+  cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+};
+
+carritoCounter();
